e62fb479f6e204036b020c2a41e2f83c484b44ce
unknown <matthijs@groov.nl>
2014-04-29 20:37:31 +0200

    oinmit

---
+++ OwinTest/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace OwinTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        app.Run(Invoke);
+        }
+
+        // Invoked once per request.
+        public Task Invoke(IOwinContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            return context.Response.WriteAsync("Hello World");
+        }
+    }
+}
