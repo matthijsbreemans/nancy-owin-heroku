a393efbb95c3ae1615e2b630d2b0aa4f45c377f6
unknown <matthijs@groov.nl>
2014-04-29 20:55:40 +0200

    fix

--- OwinTest/Program.cs
+++ OwinTest/Program.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
 
 namespace OwinTest
 {
     class Program
     {
+        private static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
         static void Main(string[] args)
         {
+            var port = 5000;
+            if (args.Length > 0)
+            {
+                int.TryParse(args[0], out port);
+            }
+
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
+
+            using (WebApp.Start<Startup>(string.Format("http://*:{0}", port)))
+            {
+                Console.WriteLine("Started");
+                _quitEvent.WaitOne();
+            }
         }
     }
 }
